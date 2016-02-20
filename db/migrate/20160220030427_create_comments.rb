59aecb4a3d332974b271dcdaf2887dd269eba9e1
Cliff Minks <cliff.minks@gmail.com>
2016-02-19 21:08:17 -0600

    Generate Comment model

--- db/migrate/20160220030427_create_comments.rb
+++ db/migrate/20160220030427_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :comenter
+      t.string :commenter
       t.text :body
       t.references :article, index: true, foreign_key: true
 
